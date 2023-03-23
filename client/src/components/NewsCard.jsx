c7d3c9504fcb25393364b72dda610ca951837dd8
sacultang <sacultang@gmail.com>
2023-03-23 19:17:46 +0900

    refactor : NewsCard 중복코드 묶음

--- client/src/components/NewsCard.jsx
+++ client/src/components/NewsCard.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react"
+import { useSelector } from "react-redux"
 import {
   Card,
   MainContent,
@@ -10,39 +10,35 @@ import {
   CardHeader,
   Tickers,
   NoResultTickers,
-} from "@styles/newsCard/cardStyles";
+} from "@styles/newsCard/cardStyles"
 import {
   ScrapIcon,
   TranslateIconKo,
   ShareIcon,
   ExpandMoreIcon,
   TranslateIconEn,
-} from "@styles/svgIcon";
-import globalMOYAPremiumSvg from "@assets/globalMOYA.svg";
-import { differenceDayFuncTwo } from "../util/dateFunc";
-import { translateApi } from "../api/translateApi";
-import ErrorMsg from "./ErrorMsg";
-import { bookmarkAll } from "../api/bookmarkApi";
-import { toggleScrapMoveBtn } from "@redux/modalSlice";
-import { addNewsID } from "../redux/reducer/scrapNewsSlice";
-import { useDispatch } from "react-redux";
-import _ from "lodash";
+} from "@styles/svgIcon"
+import globalMOYAPremiumSvg from "@assets/globalMOYA.svg"
+import { differenceDayFuncTwo } from "../util/dateFunc"
+import { translateApi } from "../api/translateApi"
+import ErrorMsg from "./ErrorMsg"
+import { toggleScrapMoveBtn } from "@redux/modalSlice"
+import { addNewsID } from "../redux/reducer/scrapNewsSlice"
+import { useDispatch } from "react-redux"
+import _ from "lodash"
 const NewsCard = ({ view, apply, newsList, errorMsg, lastElementRef }) => {
-  const [scrap, setScrap] = useState(false);
-  const [open, setOpen] = useState({});
-  const [trakingId, setTrakingId] = useState({});
-  const [translate, setTranslate] = useState([]);
-  const [changeTrans, setChangeTrans] = useState(true);
-  const viewType = useSelector((state) => state.cardTypeSlice.viewType);
-  const dispatch = useDispatch();
-  const showScrapMoveBtn = useSelector(
-    (state) => state.modalSlice.showScrapMoveBtn
-  );
+  const [scrap, setScrap] = useState(false)
+  const [open, setOpen] = useState({})
+  const [trakingId, setTrakingId] = useState({})
+  const [translate, setTranslate] = useState([])
+  const viewType = useSelector((state) => state.cardTypeSlice.viewType)
+  const dispatch = useDispatch()
+  const showScrapMoveBtn = useSelector((state) => state.modalSlice.showScrapMoveBtn)
   const handleExpand = (e) => {
-    setOpen({ [e.target.id]: !open[e.target.id] });
-  };
-  const fetch = async (newsId) => {
-    const response = await translateApi(newsId);
+    setOpen({ [e.target.id]: !open[e.target.id] })
+  }
+  const translateFetch = async (newsId) => {
+    const response = await translateApi(newsId)
     if (response.status === 200) {
       setTranslate((prev) => [
         ...prev,
@@ -51,7 +47,7 @@ const NewsCard = ({ view, apply, newsList, errorMsg, lastElementRef }) => {
           description: response.data.description,
           title: response.data.title,
         },
-      ]);
+      ])
     } else if (response.status === 400) {
       setTranslate((prev) => [
         ...prev,
@@ -60,238 +56,115 @@ const NewsCard = ({ view, apply, newsList, errorMsg, lastElementRef }) => {
           description: "description",
           title: response.data.title,
         },
-      ]);
+      ])
     }
-  };
-  const getBookmark = async () => {
-    const res = await bookmarkAll();
-    // console.log(res);
-  };
-  useEffect(() => {
-    getBookmark();
-  }, []);
-  useEffect(() => {
-    // console.log(translate);
-  }, [translate, changeTrans]);
+  }
 
   const handleTranslate = (e, newsId) => {
     if (translate.some((title) => title.newsId === newsId)) {
-      setTranslate((prev) => prev.filter((item) => item.newsId !== newsId));
-      return;
+      setTranslate((prev) => prev.filter((item) => item.newsId !== newsId))
+      return
     }
-
-    setTrakingId({ [e.target.id]: !trakingId[e.target.id] });
-    fetch(newsId);
-  };
+    setTrakingId({ [e.target.id]: !trakingId[e.target.id] })
+    translateFetch(newsId)
+  }
   const handleScrap = (newsId) => {
-    dispatch(addNewsID(newsId));
-    dispatch(toggleScrapMoveBtn(!showScrapMoveBtn));
-  };
+    dispatch(addNewsID(newsId))
+    dispatch(toggleScrapMoveBtn(!showScrapMoveBtn))
+  }
+  const getTranslatedData = (newsId, key) => {
+    if (translate.some((title) => newsId === title.newsId)) {
+      return translate.find((item) => item.newsId === newsId)[key]
+    } else {
+      return null
+    }
+  }
   return (
     <>
       {newsList.length > 0 ? (
         newsList.map((news, idx) => {
-          if (newsList.length === idx + 1) {
-            return (
-              <Card key={news.newsId} ref={lastElementRef}>
-                <MainContent viewType={apply ? view : viewType}>
-                  <ImageContent
-                    src={news.imageUrl ? news.imageUrl : globalMOYAPremiumSvg}
-                    viewType={apply ? view : viewType}
-                  />
-                  <CardHeader viewType={apply ? view : viewType}>
-                    {translate &&
-                    translate.some((title) => news.newsId === title.newsId) ? (
-                      <h2>
-                        {" "}
-                        {
-                          translate.find((item) => item.newsId === news.newsId)
-                            .title
-                        }
-                      </h2>
-                    ) : (
-                      <h2> {news.title}</h2>
-                    )}
-                  </CardHeader>
-                </MainContent>
+          const translatedTitle = getTranslatedData(news.newsId, "title")
+          const translatedDescription = getTranslatedData(news.newsId, "description")
+          return (
+            <Card key={news.newsId} ref={lastElementRef}>
+              <MainContent viewType={apply ? view : viewType}>
+                <ImageContent
+                  src={news.imageUrl ? news.imageUrl : globalMOYAPremiumSvg}
+                  viewType={apply ? view : viewType}
+                />
+                <CardHeader viewType={apply ? view : viewType}>
+                  {translatedTitle ? <h2>{translatedTitle}</h2> : <h2> {news.title}</h2>}
+                </CardHeader>
+              </MainContent>
 
-                <Abstract>
-                  {translate &&
-                  translate.some((title) => news.newsId === title.newsId) ? (
-                    <p>
-                      {
-                        translate.find((item) => item.newsId === news.newsId)
-                          .description
-                      }
-                    </p>
-                  ) : (
-                    <p> {news.description ? news.description : "no text"}</p>
-                  )}
-                </Abstract>
-
-                <SubContent>
-                  <div className="time">
-                    {news.brandName} | {differenceDayFuncTwo(news.publishTime)}
-                  </div>
-                  <div className="iconGroup">
-                    {translate &&
-                    translate.some((title) => news.newsId === title.newsId) ? (
-                      <TranslateIconEn
-                        id={news.newsId}
-                        onClick={(e) => {
-                          handleTranslate(e, news.newsId);
-                        }}
-                      />
-                    ) : (
-                      <TranslateIconKo
-                        id={news.newsId}
-                        onClick={(e) => {
-                          handleTranslate(e, news.newsId);
-                        }}
-                      />
-                    )}
-
-                    <ShareIcon />
-                    <ScrapIcon
-                      onClick={() => handleScrap(news.newsId)}
-                      $scrap={scrap}
-                    />
-                  </div>
-                </SubContent>
-                {news.assetTags && news.assetTags.length > 0 ? (
-                  <CardFooter>
-                    <Tickers $expand={`${open[idx] ? "expand" : "none"}`}>
-                      {news.nluLabels.slice(0, 3).map((label, index) => (
-                        <li key={label + index}>
-                          <strong>Related Symbols</strong> {label}
-                        </li>
-                      ))}
-                    </Tickers>
-
-                    <div className="tags">
-                      {news.assetTags.map((tag, index) => (
-                        <span key={tag + index}>#{tag}</span>
-                      ))}
-                    </div>
-
-                    <ExpandMoreIcon
-                      id={idx}
-                      onClick={(e) => {
-                        handleExpand(e, idx);
-                      }}
-                      $expand={`${open[idx] ? "expand" : "none"}`}
-                    />
-                  </CardFooter>
+              <Abstract>
+                {translatedDescription ? (
+                  <p>{translatedDescription}</p>
                 ) : (
-                  <NoResultTickers />
+                  <p> {news.description ? news.description : "no text"}</p>
                 )}
-              </Card>
-            );
-          } else {
-            return (
-              <Card key={news.newsId} ref={lastElementRef}>
-                <MainContent viewType={apply ? view : viewType}>
-                  <ImageContent
-                    src={news.imageUrl ? news.imageUrl : globalMOYAPremiumSvg}
-                    viewType={apply ? view : viewType}
-                  />
-                  <CardHeader viewType={apply ? view : viewType}>
-                    {translate &&
-                    translate.some((title) => news.newsId === title.newsId) ? (
-                      <h2>
-                        {
-                          translate.find((item) => item.newsId === news.newsId)
-                            .title
-                        }
-                      </h2>
-                    ) : (
-                      <h2> {news.title}</h2>
-                    )}
-                  </CardHeader>
-                </MainContent>
+              </Abstract>
 
-                <Abstract>
-                  {translate &&
-                  translate.some((title) => news.newsId === title.newsId) ? (
-                    <p>
-                      {
-                        translate.find((item) => item.newsId === news.newsId)
-                          .description
-                      }
-                    </p>
-                  ) : (
-                    <p>
-                      {" "}
-                      {news.description ? news.description : "no description"}
-                    </p>
-                  )}
-                </Abstract>
-
-                <SubContent>
-                  <div className="time">
-                    {news.brandName} | {differenceDayFuncTwo(news.publishTime)}
-                  </div>
-                  <div className="iconGroup">
-                    {translate &&
-                    translate.some((title) => news.newsId === title.newsId) ? (
-                      <TranslateIconEn
-                        id={news.newsId}
-                        onClick={(e) => {
-                          handleTranslate(e, news.newsId);
-                        }}
-                      />
-                    ) : (
-                      <TranslateIconKo
-                        id={news.newsId}
-                        onClick={(e) => {
-                          handleTranslate(e, news.newsId);
-                        }}
-                      />
-                    )}
-
-                    <ShareIcon />
-                    <ScrapIcon
-                      onClick={() => handleScrap(news.newsId)}
-                      $scrap={scrap}
-                    />
-                  </div>
-                </SubContent>
-                {news.assetTags && news.assetTags.length > 0 ? (
-                  <CardFooter>
-                    <Tickers $expand={`${open[idx] ? "expand" : "none"}`}>
-                      {news.nluLabels.slice(0, 3).map((label, index) => (
-                        <li key={label + index}>
-                          <strong>Related Symbols</strong> {label}
-                        </li>
-                      ))}
-                    </Tickers>
-
-                    <div className="tags">
-                      {news.assetTags.map((tag, index) => (
-                        <span key={tag + index}>#{tag}</span>
-                      ))}
-                    </div>
-
-                    <ExpandMoreIcon
-                      id={idx}
+              <SubContent>
+                <div className="time">
+                  {news.brandName} | {differenceDayFuncTwo(news.publishTime)}
+                </div>
+                <div className="iconGroup">
+                  {translate && translate.some((title) => news.newsId === title.newsId) ? (
+                    <TranslateIconEn
+                      id={news.newsId}
                       onClick={(e) => {
-                        handleExpand(e, idx);
+                        handleTranslate(e, news.newsId)
                       }}
-                      $expand={`${open[idx] ? "expand" : "none"}`}
                     />
-                  </CardFooter>
-                ) : (
-                  <NoResultTickers />
-                )}
-              </Card>
-            );
-          }
+                  ) : (
+                    <TranslateIconKo
+                      id={news.newsId}
+                      onClick={(e) => {
+                        handleTranslate(e, news.newsId)
+                      }}
+                    />
+                  )}
+
+                  <ShareIcon />
+                  <ScrapIcon onClick={() => handleScrap(news.newsId)} $scrap={scrap} />
+                </div>
+              </SubContent>
+              {news.assetTags && news.assetTags.length > 0 ? (
+                <CardFooter>
+                  <Tickers $expand={`${open[idx] ? "expand" : "none"}`}>
+                    {news.nluLabels.slice(0, 3).map((label, index) => (
+                      <li key={label + index}>
+                        <strong>Related Symbols</strong> {label}
+                      </li>
+                    ))}
+                  </Tickers>
+
+                  <div className="tags">
+                    {news.assetTags.map((tag, index) => (
+                      <span key={tag + index}>#{tag}</span>
+                    ))}
+                  </div>
+
+                  <ExpandMoreIcon
+                    id={idx}
+                    onClick={(e) => {
+                      handleExpand(e, idx)
+                    }}
+                    $expand={`${open[idx] ? "expand" : "none"}`}
+                  />
+                </CardFooter>
+              ) : (
+                <NoResultTickers />
+              )}
+            </Card>
+          )
         })
       ) : (
         <ErrorMsg errorMsg={errorMsg} />
       )}
     </>
-  );
-};
+  )
+}
 
-export default NewsCard;
+export default NewsCard
